48c32d867e484e07adfab4642ade06eb42a43c18
sodeeq olapade <olapadeabiodun21@email.com>
2019-04-26 14:28:01 +0100

    Implement Doctor Constructor
    Also Implement the actions Doctors can perform
    as methods on Doctor's prototype

---
+++ main/doctor.js
@@ -0,0 +1,43 @@
+const { MedicalStaff } = require("../main/medicalStaff");
+const { Patient } = require("../main/patient");
+const { patientDatabase, ailmentAndDrugs } = require("../storage/dataBase");
+const { Nurse } = require("../main/nurse");
+
+function Doctor(name, gender) {
+  MedicalStaff.call(this, name, gender);
+  this.role = "doctor";
+
+  console.log(`Doctor successfully created`);
+  console.log(this);
+}
+
+Doctor.prototype = Object.create(MedicalStaff.prototype);
+Doctor.prototype.constructor = Doctor;
+
+Doctor.prototype.dischargePatient = function(patientId) {
+  Patient.prototype.discharge(this.name, patientId);
+};
+
+Doctor.prototype.prescribeDrugsToPatient = function(patientId) {
+  let patientAilment = patientDatabase.find(
+    patient => patient.patientId === patientId
+  ).ailment;
+
+  let prescribedDrugs = ailmentAndDrugs.find(
+    ailment => ailment.name === patientAilment
+  ).drugs;
+
+  let medicalAdvice = ailmentAndDrugs.find(
+    ailment => ailment.name === patientAilment
+  ).medicalAdvice;
+
+  console.log(`Patient shold be given ${prescribedDrugs}`);
+  console.log(`Medical Advice: ${medicalAdvice}`);
+};
+
+Doctor.prototype.referPatientToSpecialist = function(patientId) {
+  Patient.prototype.refer(patientId);
+};
+
+
+module.exports = { Doctor };
